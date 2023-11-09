10a75547b5cd25e8059bb8e9022b762798653750
Alexdang95 <150396126+Alexdang95@users.noreply.github.com>
2023-11-09 22:27:23 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+alert('Hello World!');
+
+let emailElement = document.querySelector('#email');
+let messageElement = document.querySelector('#message');
+let submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	let emailValue = emailElement.value;
+	let messageValue = emailElement.value;
+
+	if (emailValue.includes('@')) {
+		//all good
+		alert('Thank you for your message!')
+	} else {
+		alert('Ooops! Email address is not valid, please try again')
+	}
+})
+
